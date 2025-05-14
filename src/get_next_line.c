50ce15c5274903b994be5d93c265c33fb809773c
Mika Yli-Pentti <mika.ylipentti@gmail.com>
2025-05-14 20:32:17 +0300

    Added prototypes

---
+++ src/get_next_line.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: myli-pen <myli-pen@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/14 20:14:52 by myli-pen          #+#    #+#             */
+/*   Updated: 2025/05/14 20:25:15 by myli-pen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+/**
+ * Returns a line read from file descriptor `fd`.
+ *
+ * @param fd File descriptor to read from.
+ * @return String from the read line or NULL in case of error or if nothing
+ * was read.
+ */
+char	*get_next_line(int fd)
+{
+
+}
