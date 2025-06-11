413154fc00a0db564e1a43ce1243edec3b6c1456
Mika Yli-Pentti <mika.ylipentti@gmail.com>
2025-06-11 18:39:56 +0300

    clean2

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: myli-pen <myli-pen@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/10 21:29:34 by myli-pen          #+#    #+#             */
-/*   Updated: 2025/06/11 18:04:07 by myli-pen         ###   ########.fr       */
+/*   Updated: 2025/06/11 18:28:10 by myli-pen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,52 +14,52 @@
 #include <stdio.h>
 
 // MANDATORY
-// #include "get_next_line.h"
-// int	main(void)
-// {
-// 	int		fd;
-// 	char	*str;
-// 	char	*file = "file.txt";
-
-// 	printf("MANDATORY\n");
-// 	fd = open(file, O_RDONLY);
-// 	while ((str = get_next_line(fd)))
-// 	{
-// 		printf("%s", str);
-// 		free(str);
-// 	}
-// 	close(fd);
-// 	return (0);
-// }
-
-// BONUS
-#include "get_next_line_bonus.h"
+#include "get_next_line.h"
 int	main(void)
 {
-	int		fd_bonus_1;
-	int		fd_bonus_2;
-	char	*str_bonus_1;
-	char	*str_bonus_2;
-	char	*file_bonus_1 = "file_bonus_1.txt";
-	char	*file_bonus_2 = "file_bonus_2.txt";
+	int		fd;
+	char	*str;
+	char	*file = "file.txt";
 
-	printf("BONUS\n");
-	fd_bonus_1 = open(file_bonus_1, O_RDONLY);
-	fd_bonus_2 = open(file_bonus_2, O_RDONLY);
-	while (1)
+	printf("\033[33mMANDATORY\033[0m\n");
+	fd = open(file, O_RDONLY);
+	while ((str = get_next_line(fd)))
 	{
-		str_bonus_1 = get_next_line(fd_bonus_1);
-		if (str_bonus_1)
-			printf("%s", str_bonus_1);
-		free(str_bonus_1);
-		str_bonus_2 = get_next_line(fd_bonus_2);
-		if (str_bonus_2)
-			printf("%s", str_bonus_2);
-		free(str_bonus_2);
-		if (!str_bonus_1 && !str_bonus_2)
-			break ;
+		printf("%s", str);
+		free(str);
 	}
-	close(fd_bonus_1);
-	close(fd_bonus_2);
+	close(fd);
 	return (0);
 }
+
+// BONUS
+// #include "get_next_line_bonus.h"
+// int	main(void)
+// {
+// 	int		fd_bonus_1;
+// 	int		fd_bonus_2;
+// 	char	*str_bonus_1;
+// 	char	*str_bonus_2;
+// 	char	*file_bonus_1 = "file_bonus_1.txt";
+// 	char	*file_bonus_2 = "file_bonus_2.txt";
+
+// 	printf("\033[33mBONUS\033[0m\n");
+// 	fd_bonus_1 = open(file_bonus_1, O_RDONLY);
+// 	fd_bonus_2 = open(file_bonus_2, O_RDONLY);
+// 	while (1)
+// 	{
+// 		str_bonus_1 = get_next_line(fd_bonus_1);
+// 		if (str_bonus_1)
+// 			printf("%s", str_bonus_1);
+// 		free(str_bonus_1);
+// 		str_bonus_2 = get_next_line(fd_bonus_2);
+// 		if (str_bonus_2)
+// 			printf("%s", str_bonus_2);
+// 		free(str_bonus_2);
+// 		if (!str_bonus_1 && !str_bonus_2)
+// 			break ;
+// 	}
+// 	close(fd_bonus_1);
+// 	close(fd_bonus_2);
+// 	return (0);
+// }
